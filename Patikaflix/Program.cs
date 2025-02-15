a8c9f0c65ef552f85a603504de7f4a4ef37ad9e4
abdndmn <abdndmn@hotmail.com>
2025-02-15 04:29:53 +0300

    Add project files.

---
+++ Patikaflix/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using Patikaflix;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Series> seriesList = new List<Series>();
+        string answer;
+
+        do
+        {
+            Console.WriteLine("Dizinin Adını Giriniz:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Dizinin Yapım Yılını Giriniz:");
+            var productionYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dizinin Türünü Giriniz:");
+            var genre = Console.ReadLine();
+
+            Console.WriteLine("Dizinin Yayınlanmaya Başladığı Yılı Giriniz:");
+            var releaseYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dizinin Yönetmenini Giriniz:");
+            var director = Console.ReadLine();
+
+            Console.WriteLine("Dizinin İlk Yayınlandığı Platformu Giriniz:");
+            var firstPlatform = Console.ReadLine();
+
+            seriesList.Add(new Series
+            {
+                Name = name,
+                ProductionYear = productionYear,
+                Genre = genre,
+                ReleaseYear = releaseYear,
+                Director = director,
+                FirstPlatform = firstPlatform
+            });
+
+            Console.WriteLine("Yeni Bir Dizi Eklemek İster misiniz? (E/H):");
+            answer = Console.ReadLine();
+
+        } while (answer is "E" or "e");
+
+        Console.WriteLine("Oluşturduğunuz Dizi:");
+        foreach (var item in seriesList)
+        {
+            Console.WriteLine($"Ad: {item.Name}, Yapım Yılı: {item.ProductionYear}, Tür: {item.Genre}, Yayın Yılı: {item.ReleaseYear}, Yönetmen: {item.Director}, Platform: {item.FirstPlatform}");
+        }
+
+        var newList = seriesList
+                .Where(m => m.Genre.ToLower().Contains("komedi"))
+                .Select(m => new NewSeries { Name = m.Name, Genre = m.Genre, Director = m.Director })
+                .OrderBy(m => m.Name) 
+                .ThenBy(m => m.Director)
+                .ToList();
+
+        Console.WriteLine("\nKomedi Dizileri Listesi:");
+        foreach (var item in newList)
+        {
+            Console.WriteLine($"Dizi Adı: {item.Name}, Tür: {item.Genre}, Yönetmen: {item.Director}");
+        }
+
+    }
+}
---
+++ Patikaflix/Series.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patikaflix
+{
+    internal class Series
+    {
+        public string Name { get; set; }
+
+        public int ProductionYear { get; set; }
+        public string Genre { get; set; }
+
+        public int ReleaseYear { get; set; }
+        public string Director { get; set; }
+        public string FirstPlatform { get; set; }
+
+
+    }
+}
